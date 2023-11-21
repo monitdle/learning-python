7d9090037989bd6120d604a0c90d34cca4121d80
monitdle <148724578+monitdle@users.noreply.github.com>
2023-11-21 01:08:32 +0100

    Commenting SUBS.py

--- Rosalind/SUBS.py
+++ Rosalind/SUBS.py
@@ -13,12 +13,16 @@ short_string = "ATAT"
 
 #Idea:
 #[0, n], [0 +1, n + 1],…   for n = len(short_string)
+#so we need to "scan" the longer string with a scanner that has the length of the shorter string
 
-next_position = 0
-for i in range(len(long_string)):
-    first_position = 0 + next_position
-    substring = long_string[first_position : len(short_string) + next_position]
-    if substring == short_string:
-        print(first_position + 1, end = " ")
+
+next_position = 0    #variable to move the scanner
+
+for i in range(len(long_string)):    #scanning the long string
+    first_position = 0 + next_position    #defining the first position as 0 plus the movement we need to reach the next scan
+    #section that's currently getting scanned:
+    substring = long_string[first_position : len(short_string) + next_position]   #start = defined first position, end = length of short string + movement of scanner 
+    if substring == short_string:    #if the scanned section matches the shorter string
+        print(first_position + 1, end = " ")    #print the first position aka starting point of our scanned section, with space as separator
     next_position += 1
             
