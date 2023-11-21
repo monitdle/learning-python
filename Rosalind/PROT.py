916d593da4527284149e835c083f301a670b86cc
monitdle <148724578+monitdle@users.noreply.github.com>
2023-11-21 02:03:04 +0100

    Solution to PROT

---
+++ Rosalind/PROT.py
@@ -0,0 +1,39 @@
+ 
+codontable = {"CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L", "UUA":"L", "UUG":"L", 
+            "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R", "AGA":"R", "AGG":"R", 
+            "GCC":"A", "GCU":"A", "GCA":"A", "GCG":"A", 
+            "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T", 
+            "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S", 
+            "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V", 
+            "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G", 
+            "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P", 
+            "AUU":"I", "AUC":"I", "AUA":"I", 
+            "UUU":"F", "UUC":"F", 
+            "GAU":"D", "GAC":"D", 
+            "GAA":"E", "GAG":"E", 
+            "AGU":"S", "AGC":"S", 
+            "AAA":"K", "AAG":"K", 
+            "AAU":"N", "AAC":"N", 
+            "CAU":"H", "CAC":"H", 
+            "UAU":"Y", "UAC":"Y", 
+            "UGU":"C", "UGC":"C", 
+            "CAA":"Q", "CAG":"Q", 
+            "UGG":"W", 
+            "AUG":"M", 
+            "UAA":"Stop", "UAG":"Stop", "UGA":"Stop" 
+            }
+
+# Example:
+RNAstring = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+
+movement = 0
+for i in range(len(RNAstring)):
+    start = 0 + movement
+    segment = RNAstring[start : 3 + movement]
+    if segment in codontable:
+        if codontable[segment] == "Stop":
+            break
+        else:
+            print(codontable[segment], end = "")
+            movement += 2
+    movement += 1
