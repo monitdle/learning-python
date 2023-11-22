172ba5a47927b78f84d79899e43ae6f5c6883e69
monitdle <148724578+monitdle@users.noreply.github.com>
2023-11-22 10:29:01 +0100

    Update comment in IPRB.py

--- Rosalind/IPRB.py
+++ Rosalind/IPRB.py
@@ -10,7 +10,7 @@ n = 2   #aa
 def dominant(k, m, n):
     pop = k + m + n     #population size
 
-    #branches: probability of e.g. k as one mate, times sum of probabilities of k/m/n as their mate 
+    #branches: probability of e.g. k as one mate, times sum of probabilities of k/m/n as their mate, incl. the prob of inheriting the allele
     k_branch = (k / pop) * (((k-1) / (pop-1)) + (m / (pop - 1)) + (n / (pop - 1)))
     m_branch = (m / pop) * (((3*(m-1)) / (4*(pop-1))) + (k / (pop - 1)) + (n / (2*(pop - 1))))
     n_branch = (n / pop) * ((k / (pop - 1)) + (m / (2*(pop - 1))))
