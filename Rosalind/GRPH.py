3936c3528ef64967e180ba01dceeb4404f197b71
monitdle <148724578+monitdle@users.noreply.github.com>
2023-11-25 22:23:41 +0100

    Solution to GRPH
    Step 1 and 2 are copied from GC solution

---
+++ Rosalind/GRPH.py
@@ -0,0 +1,56 @@
+# Overlap Graphs
+
+## Example
+DataRaw = """>Rosalind_0498
+AAATA
+AA
+>Rosalind_2391
+AAA
+TTTT
+>Rosalind_2323
+TTTTCCC
+>Rosalind_0442
+AAATCCC
+>Rosalind_5013
+GGGTGGG"""
+
+### Step 1: Splitting & creating lists
+DataString = DataRaw.split("\n")    #split in lines
+DataString_remv = DataRaw.replace(">", "").split("\n")
+
+
+
+### Step 2: Creating lists of DNA strings and IDs - ATTENTION: one string can take multiple lines
+DNAstrings = []
+subseq = ""
+for item in DataString[1:]:
+    if item[0] != ">":  #lines without > get recognized as DNA strings
+        subseq += item
+    else:
+        DNAstrings.append(subseq)   #>-lines signal the end of a DNA string & append to list
+        subseq = ""
+        continue
+DNAstrings.append(subseq)   #append last DNA string
+#print(DNAstrings)
+
+
+IDlist = []
+for ID in DataString_remv:
+    if "Rosalind_" in ID:   #every line with "Rosalind_" gets recognized as an ID line
+        IDlist.append(ID)  
+#print(IDlist)
+
+
+
+### Step 3: Create dictionary(?) with DNA strings as keys and ID as values
+DNA_ID = dict(zip(DNAstrings, IDlist))
+
+
+
+### Step 4.1: Comparing first 3 aminoacids with last three aminoacids of another string
+### Step 4.2: Printing out the paired DNA strings' IDs
+for i in range(len(DNAstrings)):
+    for DNA in DNAstrings:
+        if DNAstrings[i] != DNA and DNAstrings[i][-3 : ] == DNA[0 : 3]:
+            print(DNA_ID[DNAstrings[i]], DNA_ID[DNA])
+
