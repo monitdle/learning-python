237eecee66c45458e2089be8c4ff32ef1b46be3f
monitdle <monieverytime@gmail.com>
2023-12-03 17:55:21 +0100

    Solution to Day 1

--- Advent_of_code/Day1.py
+++ Advent_of_code/Day1.py
@@ -8,8 +8,8 @@
 #treb7uchet"""
 
 
-fasta = open("/Users/MoniLe/Desktop/Programming/learning-python/Advent_of_code/Day1_input.txt")
-rawdata = str(fasta)
+fasta = open("C:\\Users\\monik\\OneDrive\\Desktop\\Programming\\learning-python\\Advent_of_code\\Day1_input.txt")
+#rawdata = str(fasta)
 
 ##Ideas:
     
@@ -18,12 +18,94 @@ rawdata = str(fasta)
 
 list_of_numbers = []
 
-for string in rawdata.split():
+for string in fasta:
     numbers = ""
     for i, num in enumerate(string):
         if num in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             numbers += num
     if len(numbers) > 0:
+        print(numbers[0], numbers[-1])
+        list_of_numbers.append(numbers[0] + numbers[-1])
+
+n = 0
+for number in list_of_numbers:
+    n += int(number)
+print(n)
+
+
+
+##############################################################################
+################################# Part Two ###################################
+
+fasta = open("C:\\Users\\monik\\OneDrive\\Desktop\\Programming\\learning-python\\Advent_of_code\\Day1_input.txt")
+
+content = fasta.read()
+lines = content.splitlines()
+
+
+# Step 1: Make dict and then insert the number at the end of each word
+word_number = {"one" : "1", "two" : "2", "three" : "3", "four" : "4", "five" : "5", "six" : "6", "seven" : "7", "eight" : "8", "nine" : "9"}
+
+test = []
+
+for i, line in enumerate(lines):
+    #print(line)
+    for word, number in word_number.items():
+        if word in line:
+            index = line.find(word) + len(word)
+            line = line[:index] + number + line[index:]
+            #print(line, "*")
+            test.append(line)
+            #print(test, "test")
+
+
+list_of_numbers = []         
+            
+for string in test:
+    numbers = ""
+    for i, num in enumerate(string):
+        if num in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            numbers += num
+    if len(numbers) > 0:
+        #print(numbers[0], numbers[-1])
+        list_of_numbers.append(numbers[0] + numbers[-1])
+
+n = 0
+for number in list_of_numbers:
+    n += int(number)
+print(n)
+### somehow doesn't work for the big file....
+
+
+
+###############################################################################
+############################ Second Try - Sophia's Idea #######################
+
+fasta = open("C:\\Users\\monik\\OneDrive\\Desktop\\Programming\\learning-python\\Advent_of_code\\Day1_input.txt")
+
+lines = fasta.read()
+
+word_to_number = {'oneight':'18', 'threeight':'38', 'fiveight':'58',
+                  'nineight':'98', 'eightwo':'82', 'eighthree':'83',
+                  'sevenine':'79','twone':'21', 'one':'1', 'two':'2', 'three':'3',
+                  'four':'4', 'five':'5', 'six':'6', 'seven':'7',
+                  'eight':'8', 'nine':'9'}
+
+for word, number in word_to_number.items():
+    lines = lines.replace(word, number)
+#print(lines)
+
+lines = lines.split()
+
+list_of_numbers = []
+
+for string in lines:
+    numbers = ""
+    for i, num in enumerate(string):
+        if num in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            numbers += num
+    if len(numbers) > 0:
+        print(numbers[0], numbers[-1])
         list_of_numbers.append(numbers[0] + numbers[-1])
 
 n = 0
