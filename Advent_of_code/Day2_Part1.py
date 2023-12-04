2969e3d4afbe61ff2c6495a5659606d62eb9fe3a
monitdle <monieverytime@gmail.com>
2023-12-04 23:49:31 +0100

    added comments to Day1_Part1

--- Advent_of_code/Day2_Part1.py
+++ Advent_of_code/Day2_Part1.py
@@ -1,4 +1,4 @@
-file = open("/Users/MoniLe/Desktop/Programming/learning-python/Advent_of_code/Day2_input.txt")
+file = open("C:\\Users\\monik\\OneDrive\\Desktop\\Programming\\learning-python\\Advent_of_code\\Day2_input.txt", "r")
 games = (file.read()).splitlines()
 
 sum_of_games = 0
@@ -6,32 +6,31 @@ sum_of_games = 0
 for line, game in enumerate(games):
     game_parts = game.split(":")[1].strip()
     cube_games = {}
-    #mycubes = {"red" : 12, "green" : 13, "blue" : 14}
     
     tries = ""
-    tries = game_parts.replace(",", "").split(";")
+    tries = game_parts.replace(",", "").split(";")      #Games in lists, every round in a string
     #print(tries)
     
     for k, grab in enumerate(tries):
-        grab = grab.split(" ")
-        mycubes = {"red" : 12, "green" : 13, "blue" : 14}
-
+        grab = grab.split(" ")      #split every game into only words, alternating between number and colour
+        
+        mycubes = {"red" : 12, "green" : 13, "blue" : 14}   #cubes we have in the bag
         for i, word in enumerate(grab):
-            #print(word)
-            if len(word) > 2:
+            #print(word)                    #looping through words
+            if len(word) > 2:               #to exclude numbers, only incl. colours
                 #print(word, grab[i - 1], "***")
-                grabbed_cubes = int(grab[i - 1])
+                grabbed_cubes = int(grab[i - 1])    #the colours number is one index before -> grab[i - 1]
 
                 if word == "red":
-                    mycubes[word] -= grabbed_cubes
+                    mycubes[word] -= grabbed_cubes      #number of grabbed cubes substracted, per colour
                 elif word == "blue":
                     mycubes[word] -= grabbed_cubes
                 elif word == "green":
                     mycubes[word] -= grabbed_cubes
-        cube_games[f"Game {line + 1}, Grab {k + 1}"] = mycubes
+        cube_games[f"Game {line + 1}, Round {k + 1}"] = mycubes      #making dict for each Game and its round
         #print(cube_games)
         
-    number_of_false = 0
+    number_of_false = 0     #basically if even one round is negative, the number is > 0
     for game_grab, grab_dict in cube_games.items():  
         #print(grab_dict)
         if any(value < 0 for value in grab_dict.values()): 
@@ -41,8 +40,8 @@ for line, game in enumerate(games):
             #print(line, "True")
             number_of_false += 0
                
-    if number_of_false == 0:
-        add = line + 1
+    if number_of_false == 0:    #sum up all games IDs that don't have false rounds
+        add = line + 1      #IDs are the number of the line + 1, since lists start with 0
         #print(add)
         sum_of_games += add
             
--- Advent_of_code/Day2_Part2.py
+++ Advent_of_code/Day2_Part2.py
@@ -1,20 +1,20 @@
-file = open("/Users/MoniLe/Desktop/Programming/learning-python/Advent_of_code/Day2_input.txt")
+file = open("C:\\Users\\monik\\OneDrive\\Desktop\\Programming\\learning-python\\Advent_of_code\\Day2_input.txt", "r")
 games = (file.read()).splitlines()
+mycubes = {"red" : 12, "green" : 13, "blue" : 14}   ###### look here
+cube_games = {}
 
 
 for line, game in enumerate(games):
     game_parts = game.split(":")[1].strip()
-    cube_games = {}
     #mycubes = {"red" : 12, "green" : 13, "blue" : 14}
-    
+    cube_games[f"Game {line + 1}"] = {}
     tries = ""
     tries = game_parts.replace(",", "").split(";")
-    #print(tries)
-    
+    print("************************")
+
     for k, grab in enumerate(tries):
         grab = grab.split(" ")
-        mycubes = {"red" : 12, "green" : 13, "blue" : 14}   ###### look here
-
+        #print(grab)
         for i, word in enumerate(grab):
             #print(word)
             if len(word) > 2:
@@ -27,16 +27,17 @@ for line, game in enumerate(games):
                     mycubes[word] -= grabbed_cubes
                 elif word == "green":
                     mycubes[word] -= grabbed_cubes
-        cube_games[f"Game {line + 1}, Grab {k + 1}"] = mycubes
-        #print(cube_games)
+                #print(mycubes, "mycubes")
+                cube_games[f"Game {line + 1}"] += mycubes
+                print(cube_games, "cube")
+        curr_game = cube_games[f"Game {line + 1}"]
         
     min_number_of_cubes = {"red" : 0, "green" : 0, "blue" : 0}
     for game_grab, grab_dict in cube_games.items():  
-        print(grab_dict)
+        print(grab_dict, line)
         for key, value in grab_dict.items():
-            min_number_of_cubes[key] = value
-
-            ## min_number_of_cubes[key] += value
+            #min_number_of_cubes[key] = value
+            #min_number_of_cubes[key] += value
         
         ## power *= key
 
