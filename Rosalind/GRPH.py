9f180991f636bd825ecca22559107d408b80165d
Monika <lemon@Monikas-MacBook-Air.local>
2024-01-14 23:46:40 +0100

    New GRPH solution

--- Rosalind/GRPH.py
+++ Rosalind/GRPH.py
@@ -39,3 +39,36 @@ for i in range(len(DNAstrings)):
         if DNAstrings[i] != DNA and DNAstrings[i][-3 : ] == DNA[0 : 3]:
             print(DNA_ID[DNAstrings[i]], DNA_ID[DNA])
 
+
+
+
+############################################################################################################
+##################################### Fresh Start, 14.01.2024 ##############################################
+
+file = open("/Users/lemon/Desktop/Programming/learning-python/Rosalind/GRPH_input.txt", "r")
+DataRaw = file.read()
+DataString = DataRaw.split("\n")    #split in lines
+
+# Separating IDs and DNA:
+ID_DNA = {}
+
+for line in DataString:
+    
+    if ">" in line:
+        ID = line.replace(">", "")
+        ID_DNA[ID] = ""
+    
+    elif ">" not in line:
+        ID_DNA[ID] += line  #will take last newly created ID, since it always comes first
+
+
+# comparing last 3 nucleobases of one string with first 3 nb of another string
+for ID, DNA in ID_DNA.items():
+    for ID2, DNA2 in ID_DNA.items():
+        if ID != ID2 and DNA[-3:] == DNA2[:3]:
+            print(ID, ID2)
+        
+
+    
+    
+
